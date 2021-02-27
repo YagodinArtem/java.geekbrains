616311e929928519663207566c154a295540ee6f
YagodinArtem <art-yagodin@yandex.ru>
2021-02-27 12:50:56 +0300

    lesson3 HW somefixes

--- src/ru/geekbrains/lesson3/GuessTheNumber.java
+++ src/ru/geekbrains/lesson3/GuessTheNumber.java
@@ -11,6 +11,7 @@ public class GuessTheNumber {
     static int gameRangeStartPoint = 0;
     static int nextGame = 1;
     static boolean isUserWin = false;
+    static int gamerTry = 3;
 
     public static void main(String[] args) throws IOException {
         while (nextGame == 1) {
@@ -26,8 +27,8 @@ public class GuessTheNumber {
 
     private static void game() throws IOException {
         System.out.println("\nНовая игра! Вам необходимо угадать число, которое загадал компьютер от "
-                + gameRangeStartPoint + " до " + (gameRangeEndPoint-1) + " c трех попыток");
-        int gamerTry = 3;
+                + gameRangeStartPoint + " до " + (gameRangeEndPoint-1) + " c " + gamerTry + " попыток");
+        gamerTry = 3;
         int randomNumber = getRandomNumber();
         int gamerChance;
         while (gamerTry > 0) {
