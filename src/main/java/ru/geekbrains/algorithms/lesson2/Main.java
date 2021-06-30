d42ef7ceb441efdb75ed8df51375c3f60a308343
YagodinArtem <art-yagodin@yandex.ru>
2021-06-30 20:24:35 +0300

    fix lesson2 algorithms

--- src/main/java/ru/geekbrains/algorithms/lesson2/Main.java
+++ src/main/java/ru/geekbrains/algorithms/lesson2/Main.java
@@ -5,20 +5,25 @@ import java.util.concurrent.TimeUnit;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Random random = new Random();
-        MyArrayList<Integer> list = new MyArrayList<>();
-        char[] special = {'b', 'i', 's'};
+    static MyArrayList<Integer> list;
+    static Random random;
 
-        for (int i = 0; i < 100000; i++) {
-            list.add(random.nextInt(100));
-        }
+    public static void main(String[] args) {
+        list = new MyArrayList<>();
+        random = new Random();
+        char[] special = {'b', 'i', 's'};
 
         for (char c : special) {
             long start = System.currentTimeMillis();
-            sortSpeedCheck(list, c);
+            sortSpeedCheck(listInit(), c);
             System.out.println("  -- Elapsed time : " + TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis() - start) + " -- ");
         }
+    }
+
+    private static MyArrayList<Integer> listInit() {
+        list = new MyArrayList<>();
+        for (int i = 0; i < 100000; i++) list.add(random.nextInt(100));
+        return list;
     }
 
     private static <E extends Comparable<E>> void sortSpeedCheck(MyArrayList<E> list, char sortType) {
